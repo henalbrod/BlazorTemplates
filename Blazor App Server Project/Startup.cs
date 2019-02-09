036a8c3247863619da0a6988b091825159a11b1e
Henry Alberto Rodriguez Rodriguez <pegazux@gmail.com>
2019-02-08 23:11:27 -0500

    Blazor App Server Project

---
+++ Blazor App Server Project/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+
+namespace $safeprojectname$
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+                       
+            services.AddRazorComponents<Client.Startup>();
+
+            services.AddSingleton<HttpClient>();
+
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                {
+                    MediaTypeNames.Application.Octet,
+                    WasmMediaTypeNames.Application.Wasm,
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseResponseCompression();            
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}");
+            });
+
+
+            // app.UseBlazor<Client.Startup>();
+
+            // app.UseBlazorDebugging();
+
+            app.UseRazorComponents<Client.Startup>();
+            
+
+        }
+    }
+}
